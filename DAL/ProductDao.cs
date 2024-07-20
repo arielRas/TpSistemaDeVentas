8f0f92df1de838cc1be2e60b2fcfb871651a2e66
ariel <ariel.ras.90@gmail.com>
2024-07-19 22:51:31 -0300

    Se generan metodos para ProductDao

--- DAL/ProductDao.cs
+++ DAL/ProductDao.cs
@@ -11,13 +11,62 @@ namespace DAL
 {
     public class ProductDao
     {
+        public Product GetProductById(int idProduct)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyStoreDb"].ConnectionString;
+
+            string sqlQuery = @"
+                SELECT P.ID_PRODUCT,P.[NAME],PB.[NAME],PC.[NAME] 
+                FROM [PRODUCT] AS P 
+                JOIN PRODUCT_BRAND AS PB ON P.ID_BRAND = PB.ID_BRAND 
+                JOIN PRODUCT_CATEGORY AS PC ON P.ID_CATEGORY = PC.ID_CATEGORY 
+                WHERE P.ID_PRODUCT = @idProduct";
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (var command = new SqlCommand(sqlQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@idProduct", idProduct);
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                var product = new Product
+                                {
+                                    IdProduct = reader.GetInt32(0),
+                                    Name = reader.GetString(1),
+                                    Brand = reader.GetString(2),
+                                    Category = reader.GetString(3)
+                                };
+
+                                return product;
+                            }
+                            else
+                                throw new Exception("El producto seleccionado no existe");
+
+                        }
+                    }
+                }
+            }
+            catch { throw; }
+        }
+
         public List<Product> GetAllProducts()
         {
             List<Product> products = new List<Product>();
 
             string connectionString = ConfigurationManager.ConnectionStrings["MyStoreDb"].ConnectionString;
 
-            string sqlQuery = "SELECT P.ID_PRODUCT,P.[NAME],P.[DESCRIPTION],PB.[NAME],PC.[NAME] FROM [PRODUCT] AS P JOIN PRODUCT_BRAND AS PB ON P.ID_BRAND = PB.ID_BRAND JOIN PRODUCT_CATEGORY AS PC ON P.ID_CATEGORY = PC.ID_CATEGORY;";
+            string sqlQuery = @"
+                SELECT P.ID_PRODUCT,P.[NAME],PB.[NAME],PC.[NAME] 
+                FROM [PRODUCT] AS P 
+                JOIN PRODUCT_BRAND AS PB ON P.ID_BRAND = PB.ID_BRAND 
+                JOIN PRODUCT_CATEGORY AS PC ON P.ID_CATEGORY = PC.ID_CATEGORY;";
 
             try
             {
@@ -35,9 +84,8 @@ namespace DAL
                                 {
                                     IdProduct = reader.GetInt32(0),
                                     Name = reader.GetString(1),
-                                    Description = reader.GetString(2),
-                                    Brand = reader.GetString(3),
-                                    Category = reader.GetString(4)
+                                    Brand = reader.GetString(2),
+                                    Category = reader.GetString(3)
                                 };
 
                                 products.Add(product);
@@ -51,36 +99,7 @@ namespace DAL
             }
             catch { throw; }
         }
-
-        public double GetPrice(int idProduct, DateTime date) 
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MyStoreDb"].ConnectionString;
-
-            string sqlQuery = "SELECT dbo.FN_GET_PRICE(@idProduct, @date)";
-
-            try
-            {
-                using (var connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-
-                    using (var command = new SqlCommand(sqlQuery, connection))
-                    {
-                        command.Parameters.AddWithValue("@idProduct", idProduct);
-
-                        command.Parameters.AddWithValue("@date", date);
-
-                        var price = command.ExecuteScalar();
-
-                        if (price == DBNull.Value) throw new Exception("El precio del producto seleccionado no esta disponible");
-
-                        return Convert.ToDouble(price);
-                    }
-                }
-            }
-            catch { throw; }            
-        }
-
+            
         public List<Product> GetProductsByCategory(string category)
         {
             var products = new List<Product>();
@@ -88,7 +107,7 @@ namespace DAL
             string connectionString = ConfigurationManager.ConnectionStrings["MyStoreDb"].ConnectionString;
 
             string sqlQuery = @"
-                SELECT P.ID_PRODUCT,P.[NAME],P.[DESCRIPTION],PB.[NAME],PC.[NAME] 
+                SELECT P.ID_PRODUCT,P.[NAME],PB.[NAME],PC.[NAME] 
                 FROM [PRODUCT] AS P 
                 JOIN PRODUCT_BRAND AS PB ON P.ID_BRAND = PB.ID_BRAND 
                 JOIN PRODUCT_CATEGORY AS PC ON P.ID_CATEGORY = PC.ID_CATEGORY 
@@ -112,9 +131,8 @@ namespace DAL
                                 {
                                     IdProduct = reader.GetInt32(0),
                                     Name = reader.GetString(1),
-                                    Description = reader.GetString(2),
-                                    Brand = reader.GetString(3),
-                                    Category = reader.GetString(4)
+                                    Brand = reader.GetString(2),
+                                    Category = reader.GetString(3)
                                 };
 
                                 products.Add(product);
@@ -135,7 +153,7 @@ namespace DAL
             string connectionString = ConfigurationManager.ConnectionStrings["MyStoreDb"].ConnectionString;
 
             string sqlQuery = @"
-                SELECT P.ID_PRODUCT,P.[NAME],P.[DESCRIPTION],PB.[NAME],PC.[NAME] 
+                SELECT P.ID_PRODUCT,P.[NAME],PB.[NAME],PC.[NAME] 
                 FROM [PRODUCT] AS P 
                 JOIN PRODUCT_BRAND AS PB ON P.ID_BRAND = PB.ID_BRAND 
                 JOIN PRODUCT_CATEGORY AS PC ON P.ID_CATEGORY = PC.ID_CATEGORY 
@@ -159,9 +177,8 @@ namespace DAL
                                 {
                                     IdProduct = reader.GetInt32(0),
                                     Name = reader.GetString(1),
-                                    Description = reader.GetString(2),
-                                    Brand = reader.GetString(3),
-                                    Category = reader.GetString(4)
+                                    Brand = reader.GetString(2),
+                                    Category = reader.GetString(3)
                                 };
 
                                 products.Add(product);
@@ -169,6 +186,35 @@ namespace DAL
 
                             return products;
                         }
+                    }
+                }
+            }
+            catch { throw; }
+        }
+
+        public double GetPrice(int idProduct, DateTime date)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyStoreDb"].ConnectionString;
+
+            string sqlQuery = "SELECT dbo.FN_GET_PRICE(@idProduct, @date)";
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (var command = new SqlCommand(sqlQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@idProduct", idProduct);
+
+                        command.Parameters.AddWithValue("@date", date);
+
+                        var price = command.ExecuteScalar();
+
+                        if (price == DBNull.Value) throw new Exception("El precio del producto seleccionado no esta disponible");
+
+                        return Convert.ToDouble(price);
                     }
                 }
             }
--- Entity/Product.cs
+++ Entity/Product.cs
@@ -10,7 +10,6 @@ namespace Entity
     {
         public int IdProduct { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public float Price { get; set; }
         public string Brand { get; set; }
         public string Category { get; set; }
